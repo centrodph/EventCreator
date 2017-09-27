fa04681cc25fabd12f9324f12fc380e271977719
GerardoMac <centrodph@gmail.com>
2017-09-27 07:40:40 -0300

    For deploy

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
-var database = require('./config/database');
+
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration
+if (!process.env.MONGO_URL) {
+  var database = require('./config/database');
+}
 var mongoUrl = process.env.MONGO_URL || database.remoteUrl;
 mongoose.connect(mongoUrl);
 
